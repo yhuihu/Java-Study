d0da8dbfcd951a8043053f7dfa717d042c4aa8af
yhuihu <35516186+yhuihu@users.noreply.github.com>
2020-03-31 09:19:28 +0800

    枚举单例写法错误

--- design-patterns/src/single/lazy/EnumSingleton.java
+++ design-patterns/src/single/lazy/EnumSingleton.java
@@ -1,32 +1,41 @@
 package single.lazy;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author Tiger
  * @date 2020-03-08
  * @see single.lazy
  **/
 public class EnumSingleton {
-    private EnumSingleton() {
-    }
 
-    // 定义一个静态枚举类
-    static enum SingletonEnum {
-        //创建一个枚举对象，该对象天生为单例
+    enum SerEnumSingleton {
         INSTANCE;
-        private EnumSingleton enumSingleton;
+        private String content;
 
-        //私有化枚举的构造函数
-        private SingletonEnum() {
-            enumSingleton = new EnumSingleton();
+        public String getContent() {
+            return content;
         }
 
-        public EnumSingleton getInstance() {
-            return enumSingleton;
+        /**
+         * 内容测试
+         * @param content content
+         */
+        public void setContent(String content) {
+            this.content = content;
         }
+
+        SerEnumSingleton() { }
     }
 
-    // 对外暴露一个获取User对象的静态方法
-    public static EnumSingleton getInstance() {
-        return SingletonEnum.INSTANCE.getInstance();
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        SerEnumSingleton serEnumSingleton = SerEnumSingleton.INSTANCE;
+        serEnumSingleton.setContent("singleton1");
+        Class<SerEnumSingleton> serEnumSingletonClass = SerEnumSingleton.class;
+        Constructor<SerEnumSingleton> constructor=serEnumSingletonClass.getDeclaredConstructor(String.class,int.class);
+        constructor.setAccessible(true);
+        SerEnumSingleton serEnumSingleton1=constructor.newInstance("test",123);
+        System.out.println(serEnumSingleton==serEnumSingleton1);
     }
 }
