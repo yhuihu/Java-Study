3b074f3d4c8f3d451634da0a7d70aa5a0dae267a
yhuihu <35516186+yhuihu@users.noreply.github.com>
2020-03-31 09:40:58 +0800

    枚举单例写法错误

--- design-patterns/src/single/lazy/EnumSingleton.java
+++ design-patterns/src/single/lazy/EnumSingleton.java
@@ -32,6 +32,7 @@ public class EnumSingleton {
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         SerEnumSingleton serEnumSingleton = SerEnumSingleton.INSTANCE;
         serEnumSingleton.setContent("singleton1");
+        // 这段代码是执行不了的，因为反射的时候会判断类是不是枚举类型，如果是枚举类型，则会抛出异常
         Class<SerEnumSingleton> serEnumSingletonClass = SerEnumSingleton.class;
         Constructor<SerEnumSingleton> constructor=serEnumSingletonClass.getDeclaredConstructor(String.class,int.class);
         constructor.setAccessible(true);
