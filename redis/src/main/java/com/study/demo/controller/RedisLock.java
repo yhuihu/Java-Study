ead1c4b918e16854880a3b626639a8f4a67438a1
yhuihu <35516186+yhuihu@users.noreply.github.com>
2020-05-27 23:39:53 +0800

    update

--- redis/src/main/java/com/study/demo/controller/RedisLock.java
+++ redis/src/main/java/com/study/demo/controller/RedisLock.java
@@ -1,6 +1,7 @@
 package com.study.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Tiger
@@ -28,24 +29,33 @@ public class RedisLock {
 
     private final String SUCCESS = "1";
 
-    @Resource
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private RedisLock(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 
     @GetMapping("lock")
     public String lock(@RequestParam(name = "id") String key) throws InterruptedException {
         String lock = LOCK_PREFIX + key;
         String uuid = UUID.randomUUID().toString();
         while (!getLock(lock, LOCK_PREFIX, LOCK_EXPIRE)) {
-            Thread.sleep(1000);
+            Thread.sleep(100);
         }
         return "ok";
     }
 
     @GetMapping("unlock")
-    public String unlock(@RequestParam(name = "id") String key) throws InterruptedException {
+    public String unlock(@RequestParam(name = "id") String key) throws Exception {
         String lock = LOCK_PREFIX + key;
-        while(!releaseLock(lock, LOCK_PREFIX)){
-            Thread.sleep(1000);
+        AtomicInteger atomicInteger=new AtomicInteger(5);
+        while (!releaseLock(lock, LOCK_PREFIX)) {
+            if(atomicInteger.decrementAndGet()>0) {
+                Thread.sleep(100);
+            }else{
+                throw new Exception("人数过多");
+            }
         }
         return "ok";
     }
@@ -65,7 +75,8 @@ public class RedisLock {
             RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
             Object result = redisTemplate.execute(redisScript, new StringRedisSerializer(), new StringRedisSerializer(), Collections.singletonList(lockKey), value, expireTime + "");
             System.out.println(result + "-----------");
-            if (SUCCESS.equals(result)) {
+            assert result != null;
+            if (SUCCESS.equals(result.toString())) {
                 return true;
             }
         } catch (Exception e) {
@@ -85,6 +96,7 @@ public class RedisLock {
         String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
         RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
         Object result = redisTemplate.execute(redisScript, new StringRedisSerializer(), new StringRedisSerializer(), Collections.singletonList(lockKey), value);
-        return SUCCESS.equals(result);
+        assert result != null;
+        return SUCCESS.equals(result.toString());
     }
 }
--- thread/src/main/java/com/study/demo/Test.java
+++ thread/src/main/java/com/study/demo/Test.java
@@ -9,19 +9,13 @@ import java.util.concurrent.Callable;
  **/
 public class Test {
     public static void main(String[] args) throws Exception {
-        Callable<String> work1=new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println("测试方法1");
-                return "方法1执行完毕";
-            }
+        Callable<String> work1= () -> {
+            System.out.println("测试方法1");
+            return "方法1执行完毕";
         };
-        Callable<String> work2=new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                System.out.println("测试方法2");
-                return "方法2执行完毕";
-            }
+        Callable<String> work2= () -> {
+            System.out.println("测试方法2");
+            return "方法2执行完毕";
         };
         MyFutureTask<String> task1=new MyFutureTask<>(work1);
         MyFutureTask<String> task2=new MyFutureTask<>(work2);
